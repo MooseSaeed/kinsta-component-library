1e639565a4efa48bec718fb30bfd9ec804eb11c6
Mostafa Said <mostafasaid1994@gmail.com>
2023-08-28 23:18:08 +0300

    fix: :bug: fix

---
+++ src/components/greeting/greetings.js
@@ -0,0 +1,4 @@
+export { default as HelloWord } from "./HelloWord.vue";
+export { default as HelloWordTwo } from "./HelloWordTwo.vue";
+export { default as HelloWordThree } from "./HelloWordThree.vue";
+export { default as HelloWordFour } from "./HelloWordFour.vue";
